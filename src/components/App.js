1a98b68be8e986b14a81718783919656040a810d
VJ <solankivj94@gmail.com>
2021-02-19 23:26:33 +0530

    Webpack HMR error

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/index.scss";
+
+const App = () => {
+  return (
+      <main>
+        <section>
+          <h1>Webpack Module Federation</h1>
+        </section>
+      </main>
+  );
+};
+
+export default App;
